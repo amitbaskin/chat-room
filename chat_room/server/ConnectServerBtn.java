90a8aa0ff6fb251236868dfbf59413545316ab88
amitbaskin <amit.baskin@gmail.com>
2021-05-25 18:52:47 +0300

    brought back background classes

--- chat_room/server/ConnectServerBtn.java
+++ chat_room/server/ConnectServerBtn.java
@@ -6,16 +6,16 @@ import java.awt.event.ActionListener;
 
 public class ConnectServerBtn extends JButton {
     private static final String CONNECT = "Connect";
-//    ConnectServerBackground serverConnectInBackground;
+    ConnectServerBackground serverConnectInBackground;
 
     public ConnectServerBtn(final Server server){
         super(CONNECT);
-//        serverConnectInBackground = new ConnectServerBackground(server);
+        serverConnectInBackground = new ConnectServerBackground(server);
         addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-//                serverConnectInBackground.doInBackground();
-                server.execute();
+                serverConnectInBackground.doInBackground();
+//                server.execute();
             }
         });
     }
